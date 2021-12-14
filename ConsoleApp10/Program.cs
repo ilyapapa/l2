df2406c2177a30f2322057a7829dfe333665f089
maroze <grigorevaalla15@gmail.com>
2021-12-14 15:39:14 +0300

    l2

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Привет мир";
+            Console.WriteLine(hello);
+            Console.WriteLine("Добро пожаловать в C#!");
+            Console.WriteLine("Пока мир...");
+            Console.WriteLine(24.5);
+        }
+    }
+}
